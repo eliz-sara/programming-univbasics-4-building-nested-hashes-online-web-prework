c4f1033a19009548f12f74597a771e43be044562
Sara Antoine <blythejolei@gmail.com>
2019-10-28 15:27:59 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -6,14 +6,12 @@ end
 
 
 def monopoly_with_second_tier
-  monopoly = {
-    railroads: {purchase}
-  }
+  
   monopoly = {
     railroads: {
       pieces: 4,
-      names: {},
-      rent_in_dollars: {}
+      names: {"Mcdonals"},
+      rent_in_dollars: {$400}
     }
   }
 end
