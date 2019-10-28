de5150958ead3e05af908be5d42801f5de97fdbd
Sara Antoine <blythejolei@gmail.com>
2019-10-28 15:33:30 +0000

    Automatically backed up by Learn

--- spec/intro_to_ruby_hashes_lab_spec.rb
+++ spec/intro_to_ruby_hashes_lab_spec.rb
@@ -23,10 +23,10 @@ def monopoly_with_third_tier
     railroads: {purchase
       pieces: 4,
       names: {
-        :reading_railroad => {},
-        :pennsylvania_railroad => {},
-        :b_and_o_railroad => {},
-        :shortline => {}
+        :reading_railroad => {one_piece_owned},
+        :pennsylvania_railroad => {two_pieces_owned},
+        :b_and_o_railroad => {three_pieces_owned},
+        :shortline => {four_pieces_owned}
       },
       rent_in_dollars: {
         :one_piece_owned => 25,
